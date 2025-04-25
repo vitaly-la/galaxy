8a284b4ca5354d582f385dcc06723fee3c0b9fc7
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2025-04-25 15:06:25 +0100

    simple intersections

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-mod system;
-
+use std::collections::HashMap;
 use std::time::Instant;
 
 use kiss3d::light::Light;
 use kiss3d::nalgebra::Translation3;
 use kiss3d::window::Window;
 
-use system::System;
+mod system;
 
 fn main() {
-    let system = System::new(10);
-    let start = Instant::now();
-
     let mut window = Window::new("galaxy");
     window.set_light(Light::StickToCamera);
 
-    let mut spheres = vec![];
-    for i in 0..system.size {
-        let mut sphere = window.add_sphere(system.particle_radius(i));
-        sphere.set_color(0.0, 1.0, 0.0);
-        spheres.push(sphere);
+    let mut system = system::System::new();
+
+    let mut spheres = HashMap::new();
+    for i in 0..system.size() {
+        let mut sphere = window.add_sphere(1.0);
+        sphere.set_color(0.5, 0.8, 1.0);
+        spheres.insert(i, sphere);
     }
 
+    let mut start = Instant::now();
+
     while window.render() {
-        let elapsed = start.elapsed().as_secs_f64();
-        for (i, sphere) in spheres.iter_mut().enumerate() {
-            let position = system.particle_coord(i, elapsed);
-            sphere.set_local_translation(Translation3::from(position));
-        }
+        spheres.retain(|i, sphere| {
+            if system.particle_active(*i) {
+                let position = system.particle_coord(*i);
+                sphere.set_local_translation(Translation3::from(position));
+                let scale = 2.0 * system.particle_radius(*i);
+                sphere.set_local_scale(scale, scale, scale);
+                true
+            } else {
+                window.remove_node(sphere);
+                false
+            }
+        });
+
+        let elapsed = start.elapsed();
+        start += elapsed;
+        system.run(elapsed);
     }
 }
--- src/system.rs
+++ src/system.rs
@@ -1,18 +1,15 @@
+use std::collections::HashMap;
 use std::f64::consts::PI;
+use std::time::Duration;
 
 use kiss3d::nalgebra::{Rotation3, Vector3};
 
 use rand::Rng;
 
-fn ecc_from_mean(mean_anomaly: f64, ecc: f64, precision: f64) -> f64 {
+fn ecc_from_mean(mean_anomaly: f64, ecc: f64) -> f64 {
     let mut ecc_anomaly = mean_anomaly;
-    let mut old_value;
-    loop {
-        old_value = ecc_anomaly;
+    for _ in 0..10 {
         ecc_anomaly = mean_anomaly + ecc * ecc_anomaly.sin();
-        if (ecc_anomaly - old_value).abs() < precision {
-            break;
-        }
     }
     ecc_anomaly
 }
@@ -31,12 +28,15 @@ struct Particle {
 impl Particle {
     fn new() -> Self {
         let mut rng = rand::rng();
+        let ecc = 0.5;
+        let semi_major: f64 = 0.25;
+        let velocity = semi_major.powi(-2).cbrt();
         Particle {
             radius: 0.02,
             phase: 2.0 * PI * rng.random::<f64>(),
-            velocity: 2.5 + 2.5 * rng.random::<f64>(),
-            ecc: 0.75 * rng.random::<f64>(),
-            semi_major: 0.25,
+            velocity,
+            ecc,
+            semi_major,
             euler_a: 2.0 * PI * rng.random::<f64>(),
             euler_b: 2.0 * PI * rng.random::<f64>(),
             euler_g: 2.0 * PI * rng.random::<f64>(),
@@ -46,7 +46,7 @@ impl Particle {
     fn coord(&self, time: f64) -> Vector3<f64> {
         let mean_anomaly = (self.phase + self.velocity * time).rem_euclid(2.0 * PI);
 
-        let ecc_anomaly = ecc_from_mean(mean_anomaly, self.ecc, 0.0001);
+        let ecc_anomaly = ecc_from_mean(mean_anomaly, self.ecc);
 
         let beta = self.ecc / (1.0 + (1.0 - self.ecc * self.ecc).sqrt());
         let true_anomaly = ecc_anomaly
@@ -66,25 +66,60 @@ impl Particle {
 }
 
 pub struct System {
-    pub size: usize,
-    particles: Vec<Particle>,
+    particles: HashMap<usize, Particle>,
+    time: Duration,
 }
 
 impl System {
-    pub fn new(size: usize) -> Self {
-        let mut particles = vec![];
-        for _ in 0..size {
-            particles.push(Particle::new());
+    pub fn new() -> Self {
+        let mut particles = HashMap::new();
+        for i in 0..10 {
+            particles.insert(i, Particle::new());
         }
 
-        System { size, particles }
+        System {
+            particles,
+            time: Duration::ZERO,
+        }
     }
 
-    pub fn particle_coord(&self, i: usize, time: f64) -> Vector3<f32> {
-        self.particles[i].coord(time).map(|x| x as f32)
+    pub fn size(&self) -> usize {
+        self.particles.len()
+    }
+
+    pub fn particle_active(&self, i: usize) -> bool {
+        self.particles.contains_key(&i)
+    }
+
+    pub fn particle_coord(&self, i: usize) -> Vector3<f32> {
+        self.particles[&i]
+            .coord(self.time.as_secs_f64())
+            .map(|x| x as f32)
     }
 
     pub fn particle_radius(&self, i: usize) -> f32 {
-        self.particles[i].radius as f32
+        self.particles[&i].radius as f32
+    }
+
+    pub fn run(&mut self, elapsed: Duration) {
+        self.time += elapsed;
+        let mut intersections = vec![];
+        for (i, p1) in self.particles.iter() {
+            for (j, p2) in self.particles.iter() {
+                if i < j {
+                    let p1_coord = p1.coord(self.time.as_secs_f64());
+                    let p2_coord = p2.coord(self.time.as_secs_f64());
+                    let distance = (p1_coord - p2_coord).norm();
+                    if distance < p1.radius + p2.radius {
+                        intersections.push((*i, *j));
+                    }
+                }
+            }
+        }
+        for (i, j) in intersections {
+            self.particles.get_mut(&i).unwrap().radius =
+                (self.particles[&i].radius.powi(3) + self.particles[&j].radius.powi(3)).cbrt();
+            self.particles.remove(&j);
+        }
     }
 }
